6d5a6d8083db2ffc6c0c68cd7e4a7e1540bb6ebf
geoffrey jost <geoffrey.jost@densho.org>
2016-05-11 15:45:58 -0700

    correct project name in wsgi.py

--- idservice/idservice/wsgi.py
+++ idservice/idservice/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for encycrg project.
+WSGI config for idservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "encycrg.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "idservice.settings")
 
 application = get_wsgi_application()
