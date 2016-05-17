162aa51538aba5d5a91b4625a4f69daaa86ce25e
geoffrey jost <geoffrey@jostwebwerks.com>
2016-05-17 14:57:56 -0700

    create auth token whenever creating user

--- idservice/registrar/models.py
+++ idservice/registrar/models.py
@@ -1,11 +1,24 @@
 from __future__ import unicode_literals
 import json
 
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from django.core.urlresolvers import reverse
 from django.db import models
 
+from rest_framework.authtoken.models import Token
+
 from DDR import identifier
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    """Catch User post_save signal and generate authentication token.
+    """
+    if created:
+        Token.objects.create(user=instance)
 
 
 class ObjectID(models.Model):
