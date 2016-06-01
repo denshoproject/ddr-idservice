94b0f6e7436a1241ba1e027f1c91e220bfd7fa9c
geoffrey jost <geoffrey@jostwebwerks.com>
2016-05-31 17:57:45 -0700

    admin: added filtering and search

--- idservice/registrar/admin.py
+++ idservice/registrar/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from .models import ObjectID
 
-admin.site.register(ObjectID)
+
+@admin.register(ObjectID)
+class ObjectIDAdmin(admin.ModelAdmin):
+    fields = ('group', 'id', 'model',)
+    list_display = ('id', 'group', 'model',)
+    list_filter = ('group', 'model')
+    search_fields = ['id']
