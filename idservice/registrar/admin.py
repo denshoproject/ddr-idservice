f39e515fa3cab030bd83ba7dd15a3d144b78a6ea
geoffrey jost <geoffrey@jostwebwerks.com>
2017-08-07 17:56:04 -0700

    admin: list and filter on created, show created,modified in detail

--- idservice/registrar/admin.py
+++ idservice/registrar/admin.py
@@ -5,7 +5,14 @@ from .models import ObjectID
 
 @admin.register(ObjectID)
 class ObjectIDAdmin(admin.ModelAdmin):
-    fields = ('group', 'id', 'model',)
-    list_display = ('id', 'group', 'model',)
-    list_filter = ('group', 'model')
+    fields = (
+        'group',
+        'id',
+        'model',
+        'created',
+        'modified',
+    )
+    readonly_fields = ('created', 'modified',)
+    list_display = ('id', 'group', 'model', 'created',)
+    list_filter = ('group', 'model', 'created',)
     search_fields = ['id']
