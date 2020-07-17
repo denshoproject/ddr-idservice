ec93b56114cf5729d85e15919c9df2d3ea910085
Geoffrey Jost <geoffrey@jostwebwerks.com>
2020-07-17 15:52:55 -0700

    First pass at OpenAPI / Swagger

--- idservice/registrar/urls.py
+++ idservice/registrar/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from drf_yasg import views as yasg_views
+from drf_yasg import openapi
+from rest_framework import permissions
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
 
@@ -11,7 +14,26 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'objectids', views.ObjectIDViewSet)
 
+schema_view = yasg_views.get_schema_view(
+   openapi.Info(
+      title="DDR ID Service API",
+      default_version='0.1',
+      #description="DESCRIPTION TEXT HERE",
+      terms_of_service="http://ddr.densho.org/terms/",
+      contact=openapi.Contact(email="info@densho.org"),
+      #license=openapi.License(name="TBD"),
+   ),
+   #validators=['flex', 'ssv'],
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+
+    path('api/swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('api/swagger.yaml', schema_view.without_ui(cache_timeout=0), name='schema-yaml'),
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('api/0.1/objectids/<slug:oid>/next/<slug:model>/', views.next_object, name='next-object'),
     path('api/0.1/objectids/<slug:oid>/check/', views.check_ids, name='check-ids'),
