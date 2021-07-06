bc6663573cb1246e8a29797c6d7e6933f45a4860
Geoffrey Jost <geoffrey@jostwebwerks.com>
2021-07-06 10:41:17 -0700

    noidminter: GET and POST views for listing,minting NOIDs

--- idservice/noidminter/api.py
+++ idservice/noidminter/api.py
@@ -1,10 +1,15 @@
 import logging
 logger = logging.getLogger(__name__)
 
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User, Group
+from django.http import HttpResponseRedirect
+
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from rest_framework.views import APIView
+
+from . import models
 
 
 @api_view(['GET'])
@@ -15,3 +20,34 @@ def index(request, format=None):
         #'noids': reverse('nm-api-noids', request=request),
     }
     return Response(data)
+
+
+class Ark(APIView):
+
+    def get(self, request, naa, template, format=None):
+        """Returns ?limit=N most recent Noids for specified NAA and template
+        
+        limit (default 10)
+        limit=all to get all records
+        """
+        limit = int(request.GET.get('limit', '10'))
+        noids = [
+            noid.id
+            for noid in models.Noid.objects \
+                .filter(naa=naa, template=template).order_by('-n')[:limit]
+        ]
+        return Response(noids)
+
+    def post(self, request, naa, template, format=None):
+        """Get the next NOID for the specified NAA and template
+        """
+        num = int(request.POST.get('num', '1'))
+        n = models.Noid.max_n(naa, template)
+        noids = []
+        while(num):
+            num = num - 1
+            n = n + 1
+            noid = models.Noid.mint(naa, template, n)
+            noid.save()
+            noids.append(noid.id)
+        return Response(noids)
--- idservice/noidminter/urls.py
+++ idservice/noidminter/urls.py
@@ -3,6 +3,13 @@ from django.urls import include, path, re_path
 from . import api
 
 urlpatterns = [
+    
+    #path('api/1.0/<slug:thing>/', api.thing, name='nm-api-thing'),
+    #re_path(r"^api/1.0/noids/(?P<thing>[\w\W]+)/$", api.something, name='nm-api-something'),
+
+    #path('api/1.0/ark/<slug:naa>/<slug:template>/', api.Ark.as_view(), name='nm-api-ark'),
+    re_path(r"^api/1.0/(?P<naa>[\w\W]+)/(?P<template>[\w\W.]+)/$", api.Ark.as_view(), name='nm-api-ark'),
+
     path('api/1.0/', api.index, name='nm-api-index'),
     path('api/', api.index, name='nm-api-index'),
 ]
