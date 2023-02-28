ba839b81ecab9a290ccf30e60e0a84259f966a6a
Geoffrey Jost <geoffrey@jostwebwerks.com>
2023-02-28 10:46:20 -0800

    noidminter: Add admin UI

--- idservice/noidminter/admin.py
+++ idservice/noidminter/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Noid
+
+
+@admin.register(Noid)
+class ObjectIDAdmin(admin.ModelAdmin):
+    fields = (
+        'id',
+        'naan',
+        'template',
+        'created',
+        'modified',
+    )
+    readonly_fields = ('template', 'naan', 'id', 'n', 'created', 'modified',)
+    list_display = ('id', 'naan', 'template', 'created', 'modified',)
+    list_filter = ('template', 'naan', 'created', 'modified',)
+    search_fields = ['id']
